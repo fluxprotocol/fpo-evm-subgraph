70ad47c384e1370bd8b0222062d5c8d47b20a122
Jameson Hodge <contact@jamesonhodge.com>
2022-06-30 13:39:14 -0400

    feat: implement `handleNewTransmission()`

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,24 +1,21 @@
-import { BigInt } from '@graphprotocol/graph-ts'
+import { BigInt, Bytes } from '@graphprotocol/graph-ts'
 import { FluxPriceFeed as PriceFeedTemplate } from "../generated/templates"
-import { FluxPriceFeed as PriceFeedContract } from "../generated/templates/FluxPriceFeed/FluxPriceFeed"
-import { PriceFeed as PriceFeedSchema } from "../generated/schema"
+import { FluxPriceFeed as PriceFeedContract, NewTransmission } from "../generated/templates/FluxPriceFeed/FluxPriceFeed"
+import { PriceFeed as PriceFeedSchema, Transmission } from "../generated/schema"
 import {
+  FluxP2PFactory,
   PriceFeedCreated,
   PriceFeedSignersModified
 } from "../generated/FluxP2PFactory/FluxP2PFactory"
 
 export function handlePriceFeedCreated(event: PriceFeedCreated): void {
-  let entity = PriceFeedSchema.load(event.params.id)
-  if (!entity) {
-    entity = new PriceFeedSchema(event.params.id)
-  }
+  let entity = new PriceFeedSchema(event.params.oracle)
 
   let contract = PriceFeedContract.bind(event.params.oracle)
-  entity.address = event.params.oracle
   entity.pair = contract.description()
   entity.decimals = BigInt.fromI32(contract.decimals())
   entity.creator = event.transaction.from
-  entity.signers = event.params.signers
+  entity.signers = event.params.signers.map<Bytes>(s => changetype<Bytes>(s))
   
   PriceFeedTemplate.create(event.params.oracle)
 
@@ -28,7 +25,9 @@ export function handlePriceFeedCreated(event: PriceFeedCreated): void {
 export function handlePriceFeedSignersModified(
   event: PriceFeedSignersModified
 ): void {
-  let entity = PriceFeedSchema.load(event.params.id)
+  let entity = PriceFeedSchema.load(
+    FluxP2PFactory.bind(event.address).addressOfPricePair(event.params.id)
+  )
   if (!entity) {
     return
   }
@@ -36,10 +35,25 @@ export function handlePriceFeedSignersModified(
   if (event.params.isAdded == true) {
     entity.signers.push(event.params.signer)
   } else {
-    entity.signers = entity.signers.filter(
-      signer => signer !== event.params.signer
-    )
+    let index = entity.signers.indexOf(event.params.signer)
+    if (index > -1) {
+      entity.signers.splice(index, 1);
+    }
   }
+
+  entity.save()
+}
+
+export function handleNewTransmission(
+  event: NewTransmission
+): void {
+
+  let entity = new Transmission(event.transaction.hash)
+
+  entity.price_feed = event.address
+  entity.round_id = event.params.aggregatorRoundId
+  entity.answer = event.params.answer
+  entity.timestamp = event.params.timestamp
 
   entity.save()
 }
