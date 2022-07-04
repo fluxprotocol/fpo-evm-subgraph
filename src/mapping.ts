a59253a9801cdd5c7a91b951eb0f6935fb9b902c
Jameson Hodge <contact@jamesonhodge.com>
2022-07-04 11:50:45 -0400

    feat: deploy to goerli; new event without templates

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,33 +1,29 @@
 import { BigInt, Bytes } from '@graphprotocol/graph-ts'
-import { FluxPriceFeed as PriceFeedTemplate } from "../generated/templates"
-import { FluxPriceFeed as PriceFeedContract, NewTransmission } from "../generated/templates/FluxPriceFeed/FluxPriceFeed"
+import { FluxPriceFeed as PriceFeedContract } from "../generated/templates/FluxPriceFeed/FluxPriceFeed"
 import { PriceFeed as PriceFeedSchema, Transmission } from "../generated/schema"
 import {
-  FluxP2PFactory,
   PriceFeedCreated,
-  PriceFeedSignersModified
+  PriceFeedSignersModified,
+  PriceFeedTransmitted
 } from "../generated/FluxP2PFactory/FluxP2PFactory"
 
 export function handlePriceFeedCreated(event: PriceFeedCreated): void {
-  let entity = new PriceFeedSchema(event.params.oracle)
+  let entity = new PriceFeedSchema(event.params.id)
 
   let contract = PriceFeedContract.bind(event.params.oracle)
+  entity.address = event.params.oracle
   entity.pair = contract.description()
   entity.decimals = BigInt.fromI32(contract.decimals())
   entity.creator = event.transaction.from
   entity.signers = event.params.signers.map<Bytes>(s => changetype<Bytes>(s))
-  
-  PriceFeedTemplate.create(event.params.oracle)
-
   entity.save()
+
 }
 
 export function handlePriceFeedSignersModified(
   event: PriceFeedSignersModified
 ): void {
-  let entity = PriceFeedSchema.load(
-    FluxP2PFactory.bind(event.address).addressOfPricePair(event.params.id)
-  )
+  let entity = PriceFeedSchema.load(event.params.id)
   if (!entity) {
     return
   }
@@ -45,15 +41,16 @@ export function handlePriceFeedSignersModified(
 }
 
 export function handleNewTransmission(
-  event: NewTransmission
+  event: PriceFeedTransmitted
 ): void {
 
   let entity = new Transmission(event.transaction.hash)
 
-  entity.price_feed = event.address
-  entity.round_id = event.params.aggregatorRoundId
+  entity.price_feed = event.params.id
+  entity.round_id = event.params.roundId
   entity.answer = event.params.answer
   entity.timestamp = event.params.timestamp
+  entity.num_answers = event.params.numAnswers
 
   entity.save()
 }
